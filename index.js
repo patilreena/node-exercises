e08a60950bafbd27290b6f3f721dc9fe03add40f
Reena Patil <reena.patil@outlook.com>
2018-02-12 13:19:50 +1100

    setting body-parser middleware and static path

--- index.js
+++ index.js
@@ -2,8 +2,16 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
 
-const app = express();
+const app = express(); //calling a function
 
+//body-parser middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+//set a static path
+app.use(express.static(path.join(__dirname, 'public')));
+
+//registering the HTTP Handlers or API Handlers
 app.get('/', function(req, res) {
   res.send('Hello Express');
 });
